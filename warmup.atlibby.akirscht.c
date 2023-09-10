5d85a2cc7d075d6792cf482738836fee4d0f6a74
atlibby <97980657+atlibby@users.noreply.github.com>
2023-09-10 18:13:27 -0400

    Add files via upload

--- warmup.atlibby.akirscht.c
+++ warmup.atlibby.akirscht.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "warmup.atlibby.akirscht.h"
+
+Thing *createThing(char *name, unsigned int weight){
+    if (name != NULL && sizeof(name) > 0 && sizeof(name) <= MAX_NAME_LENGTH && weight > 0){
+        Thing *newThing;
+        newThing = malloc(sizeof(Thing));
+        return newThing;
+    }
+    else
+        return NULL;
+
+}
+
+int compareThings(Thing *thingOne, Thing *thingTwo){
+    
+}
 
--- warmup.atlibby.akirscht.h
+++ warmup.atlibby.akirscht.h
@@ -1,1 +1,18 @@
+//
+// Created by Andrew Libby on 9/10/23.
+//
 
+#ifndef CS3010_ASSIGNMENT1_WARMUP_ATLIBBY_AKIRSCHT_H
+#define CS3010_ASSIGNMENT1_WARMUP_ATLIBBY_AKIRSCHT_H
+
+#define MAX_NAME_LENGTH 63
+typedef struct {
+    unsigned int weight;
+    char name[1+MAX_NAME_LENGTH];
+} Thing;
+
+Thing *createThing(char *name, unsigned int weight);
+
+int compareThings(Thing *thingOne, Thing *thingTwo);
+
+#endif //CS3010_ASSIGNMENT1_WARMUP_ATLIBBY_AKIRSCHT_H
