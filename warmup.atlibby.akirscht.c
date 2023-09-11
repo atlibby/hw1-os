6e0ad9644c60546d389481cde5008ce1b2448a6f
alynkt <alynkirsch@gmail.com>
2023-09-11 00:28:33 -0400

    Corrections after testing

--- warmup.atlibby.akirscht.c
+++ warmup.atlibby.akirscht.c
@@ -4,35 +4,25 @@
 #include "warmup.atlibby.akirscht.h"
 
 Thing *createThing(char *name, unsigned int weight){
-    if (name != NULL && sizeof(name) > 0 && sizeof(name) <= MAX_NAME_LENGTH && weight > 0){
+    if (name != NULL && strlen(name) > 0 && sizeof(name) <= MAX_NAME_LENGTH && weight > 0){
         Thing *newThing;
         newThing = malloc(sizeof(Thing));
+        strcpy(newThing->name, name); // the arrow says that this is a pointer variable
+        newThing->weight = weight;
+
         return newThing;
     }
-    else
-        return NULL;
+    else{return NULL;}
 
 }
 
 int compareThings(Thing *thingOne, Thing *thingTwo){
-    thingOne = malloc(sizeof(Thing));
-    thingTwo = malloc(sizeof(Thing));
-    char name1[1 + MAX_NAME_LENGTH], name2[1 + MAX_NAME_LENGTH];
-    int res;
-    strcpy(name1, thingOne->name);
-    strcpy(name2, thingTwo->name);
-    res = strcmp(name1, name2);
-    if (res == -1)
-        return -1;
-    if (res == 1)
-        return 1;
-    if (res == 0){
-        if(thingOne->weight > thingTwo->weight)
-            return -1;
-        if(thingOne->weight < thingTwo->weight)
-            return 1;
-        if(thingOne->weight == thingTwo->weight)
-            return 0;
+    if (strcmp(thingOne->name, thingTwo->name) < 0){return -1;}
+    else if (strcmp(thingOne->name, thingTwo->name) > 0){return 1;}
+    else {
+        if (thingOne->weight < thingTwo->weight){return -1;}
+        else if (thingOne->weight > thingTwo->weight){return 1;}
+        else {return 0;}
     }
 }
 
